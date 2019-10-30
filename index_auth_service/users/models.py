e9703442b38ab95dda641ca027e295ebe47a1c98
Silver3310 <epifanov998@mail.ru>
2019-10-30 19:38:46 +0700

    Refactor users' urls and views

--- index_auth_service/users/models.py
+++ index_auth_service/users/models.py
@@ -8,7 +8,16 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = CharField(
+        _("Name of User"),
+        blank=True,
+        max_length=255
+    )
 
     def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+        return reverse(
+            "users:detail",
+            kwargs={
+                "username": self.username
+            }
+        )
--- index_auth_service/users/urls.py
+++ index_auth_service/users/urls.py
@@ -8,7 +8,19 @@ from index_auth_service.users.views import (
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path(
+        "~redirect/",
+        view=user_redirect_view,
+        name="redirect"
+    ),
+    path(
+        "~update/",
+        view=user_update_view,
+        name="update"
+    ),
+    path(
+        "<str:username>/",
+        view=user_detail_view,
+        name="detail"
+    ),
 ]
