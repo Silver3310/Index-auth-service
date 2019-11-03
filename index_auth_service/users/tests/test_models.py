4348de65e97214c0d0bd8b8128d8c5f41f953c4c
Silver3310 <epifanov998@mail.ru>
2019-11-03 17:41:29 +0700

    Add a creation test for Friendship

--- index_auth_service/users/tests/test_models.py
+++ index_auth_service/users/tests/test_models.py
@@ -1,8 +1,19 @@
 import pytest
 from django.conf import settings
 
+from index_auth_service.users.models import Friendship
+from index_auth_service.users.tests.factories import FriendshipFactory
+
 pytestmark = pytest.mark.django_db
 
 
 def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
     assert user.get_absolute_url() == f"/users/{user.username}/"
+
+
+@pytest.mark.django_db
+def test_friendship_creations():
+    """Check if friendships are created without any surprise"""
+    FriendshipFactory()
+    assert Friendship.objects.count() == 1
+
