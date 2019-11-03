865d5d9cfd076bb8d3dd65bfd185f0258ee33ed3
Silver3310 <epifanov998@mail.ru>
2019-11-03 15:20:53 +0700

    Separate the navigation panel thru template tags

---
+++ index_auth_service/users/templatetags/common.py
@@ -0,0 +1,47 @@
+from django import template
+from django.urls import resolve
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    'nav_bar.html',
+    takes_context=True
+)
+def nav_bar(context):
+    """
+    Define an active tab for the navigation bar
+    """
+
+    home_active = ''
+    about_active = ''
+    logout_active = ''
+    signup_active = ''
+    login_active = ''
+    detail_active = ''
+
+    request = context['request']
+    url_name = resolve(request.path_info).url_name
+
+    if url_name == 'home':
+        home_active = 'active'
+    elif url_name == 'about':
+        about_active = 'active'
+    elif url_name == 'account_logout':
+        logout_active = 'active'
+    elif url_name == 'account_signup':
+        signup_active = 'active'
+    elif url_name == 'account_login':
+        login_active = 'active'
+    elif url_name == 'detail':
+        detail_active = 'active'
+
+    return {
+        'request': request,
+        'home_active': home_active,
+        'about_active': about_active,
+        'logout_active': logout_active,
+        'signup_active': signup_active,
+        'login_active': login_active,
+        'detail_active': detail_active
+    }
