7afacb26e4bfcaecb38f0cdd99f3863670b5130c
Silver3310 <epifanov998@mail.ru>
2019-11-03 18:29:49 +0700

    Add one more test for Friendships

--- index_auth_service/users/tests/test_models.py
+++ index_auth_service/users/tests/test_models.py
@@ -16,4 +16,5 @@ def test_friendship_creations():
     """Check if friendships are created without any surprise"""
     FriendshipFactory()
     assert Friendship.objects.count() == 1
+    assert Friendship.objects.first().status == Friendship.WAITING_FOR_REPLY
 
