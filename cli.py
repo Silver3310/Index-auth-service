d68d454ec270c556da2456c70e2da6cccda88cba
Silver3310 <epifanov998@mail.ru>
2019-12-26 10:51:43 +0700

    Add a command line interface with click

--- cli.py
+++ cli.py
@@ -1,20 +1,72 @@
 """A Convenient Command Line Interface for common tasks"""
-
+import click
 import sys
 from subprocess import call
 
 
-def run_command(arg):
-    # run an arbitrary command
+@click.group()
+def cli():
+    """Command Line Interface for the PrivetThere project"""
+    pass
+
+
+@click.command()
+def runserver():
+    """Run the project"""
     call(
-        'docker exec -ti index-auth-service_django_1 sh -c'.split()
-        + [f"source compose/production/django/entrypoint && {arg}"]
+        'docker-compose -f local.yml up'.split()
     )
 
 
-if __name__ == "__main__":
-    command = ' '.join(sys.argv[1:])
+@click.command()
+def stop():
+    """Stop the project"""
+    call(
+        'docker-compose -f local.yml stop'.split()
+    )
 
-    run_command(command)
+
+@click.command()
+def test():
+    """Test the project"""
+    call(
+        'docker exec -ti index-auth-service_django_1 sh -c'.split()
+        + [
+            "source compose/production/django/entrypoint && pytest"
+        ]
+    )
 
 
+@click.command()
+def cov():
+    """Test the project with coverage and html report"""
+    call(
+        'docker exec -ti index-auth-service_django_1 sh -c'.split()
+        + [
+            "source compose/production/django/entrypoint && "
+            "coverage run -m pytest && coverage html"
+        ]
+    )
+
+
+@click.command(context_settings=dict(
+    allow_extra_args=True,
+))
+def rc():
+    """Run an arbitrary command"""
+    command = ' '.join(sys.argv[2:])
+    call(
+        'docker exec -ti index-auth-service_django_1 sh -c'.split()
+        + [f"source compose/production/django/entrypoint && {command}"]
+    )
+
+
+cli.add_command(runserver)
+cli.add_command(stop)
+cli.add_command(test)
+cli.add_command(cov)
+cli.add_command(rc)
+
+
+if __name__ == '__main__':
+    cli()
